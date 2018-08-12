6f2a698474d8c3f72a7c29cc3886b41253e3ea50
mahima <mahima.6.ms@gmail.com>
2018-08-12 22:21:20 +0530

    handle progress indicator visibility for refresh
    check internet connectivity before displaying the results

--- app/src/main/java/example/com/pocketnews/MainActivity.java
+++ app/src/main/java/example/com/pocketnews/MainActivity.java
@@ -1,6 +1,5 @@
 package example.com.pocketnews;
 
-import android.app.SearchManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -10,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,7 +18,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -31,7 +30,7 @@ import example.com.pocketnews.loader.NewsLoader;
 import example.com.pocketnews.model.NewsItem;
 import example.com.pocketnews.utils.Utils;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsItem>> {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsItem>>, SwipeRefreshLayout.OnRefreshListener {
 
     private static final String GDN_REQ_URL = "http://content.guardianapis.com/search";
     private static final String QUERY_API_KEY = "api-key";
@@ -49,10 +48,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     TextView emptyTextView;
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
+    @BindView(R.id.refreshLayout)
+    SwipeRefreshLayout refreshLayout;
 
     private NewsAdapter adapter;
     private List<NewsItem> news;
     private String queryText;
+    private LoaderManager loaderManager;
+    private boolean isConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,10 +65,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         ButterKnife.bind(this);
 
         //get the reference to the loader manager to handle callbacks to loader
-        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager = getSupportLoaderManager();
 
         //verify internet connectivity
-        boolean isConnected = Utils.isConnectedToNetwork(this);
+        isConnected = Utils.isConnectedToNetwork(this);
 
         //display helpful text to user if the internet connection is not working
         if (!isConnected) {
@@ -79,6 +82,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             //initialize a new loader by calling onCreateLoader, if there isn't an existing one to reuse
             loaderManager.initLoader(1, null, this);
         }
+
+        refreshLayout.setOnRefreshListener(this);
 
         //initialize the array list to contain the news
         news = new ArrayList<>();
@@ -109,7 +114,30 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             //start settings activity when settings menu option is chosen
             startActivity(new Intent(this, SettingsActivity.class));
         }
+
+        if (item.getItemId() == R.id.action_refresh) {
+            progressBar.setVisibility(View.VISIBLE);
+            onRefresh();
+        }
         return true;
+    }
+
+    /**
+     * called on swipe gesture
+     */
+    @Override
+    public void onRefresh() {
+        //verify connectivity before fetching the results
+        isConnected = Utils.isConnectedToNetwork(this);
+        if (!isConnected) {
+            emptyTextView.setText(R.string.no_internet_connection);
+            emptyTextView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_cloud_off, 0, 0);
+            emptyTextView.setCompoundDrawablePadding(8);
+            progressBar.setVisibility(View.GONE);
+            return;
+        }
+            //restart loader to reload the data
+            loaderManager.restartLoader(1, null, this);
     }
 
     /**
@@ -158,6 +186,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         //hide the progress indicator once the loading is complete
         progressBar.setVisibility(View.GONE);
+
+        //hide refresh progress indicator
+        refreshLayout.setRefreshing(false);
 
         //verify if the data loaded is empty
         if (data != null && !data.isEmpty()) {
